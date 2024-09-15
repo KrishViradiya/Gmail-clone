b44ffff520ad3ebcb69cc4c91a4b4c777bee8b5c
Krish Viradiya <krishviradiya1303@gmail.com>
2024-09-15 23:59:46 +0530

    changes in the firebase file

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2-CnS-6p7sw7lh9oFzKSxNn5CJmlSm64",
-  authDomain: "clone-65705.firebaseapp.com",
-  projectId: "clone-65705",
-  storageBucket: "clone-65705.appspot.com",
-  messagingSenderId: "876766170473",
-  appId: "1:876766170473:web:09e8dd99fd609cc23241f8",
-  measurementId: "G-JK2XYR7E73"
+  apiKey: import.meta.env.FIREBASE_API,
+  authDomain:import.meta.env.AUTH_DOMAIN ,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.MESSAGING_ID  ,
+  appId: import.meta.env.APP_ID,
+  measurementId:import.meta.env.MEASUREMENT_ID 
 };
 
 // Initialize Firebase
